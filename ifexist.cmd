bef10d944a851dc99d894f263743f501e4b503f3
dtcu0ng ( CuongZ ) <dtcu0ng@users.noreply.github.com>
2020-01-31 21:27:12 +0700

    Create ifexist.cmd

---
+++ ifexist.cmd
@@ -0,0 +1,18 @@
+@echo off
+REM TURNED OFF ECHO, ONLY SHOW OUTPUT
+REM ---------------------------------------------------------------------
+REM DEMO FOR CHECKING THAT FOLDER EXIST, ELSE MAKE NEW FOLDER.
+REM USE FOR TESTING. TESTED ON WINDOWS 10, BUILD 18362
+REM THE FOLDER HERE IS NAMED ' keys '
+REM I USUALLY FORGET SOME SIMPLE COMMAND, SO CREATED THIS GIST TO REMEMBER IT
+REM SOURCE: https://gist.github.io/CuongZ
+REM --------------------------------------------------------------------
+REM --------------------------------------------------------------------
+REM MAIN FUNC IS RUN, THE FOLDER IS ' keys '
+if exist keys/ (
+    echo folder exist
+) else (
+    mkdir keys
+    echo folder created
+)
+REM ---------------------------------------------------------------------
